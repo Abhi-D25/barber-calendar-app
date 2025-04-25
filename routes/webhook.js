d9c8fff5803508bcbc9e8037f6f43e7aebb4cb9f
Abhi Dutta <duabhirup@gmail.com>
2025-04-25 16:02:16 -0700

    Conversation Storage functionality

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { google } = require('googleapis');
-const { barberOps, clientOps, appointmentOps, supabase } = require('../utils/supabase');
+const { barberOps, clientOps, appointmentOps, conversationOps, supabase } = require('../utils/supabase');
 
 const createOAuth2Client = (refreshToken) => {
   const oauth2Client = new google.auth.OAuth2(
